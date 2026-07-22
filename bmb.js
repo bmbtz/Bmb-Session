7b867a9e2c9272a65873f790f4913ebf07fcf2ff
Bmb Tech <hanchobmb@gmail.com>
2026-07-22 07:59:15 +0300

    Change QR page file served from qr_page.html to qr.html

--- bmb.js
+++ bmb.js
@@ -9,7 +9,7 @@ const express = require('express');
   app.use('/qr/code', server);
   app.use('/code', code);
   app.use('/qr', async (req, res, next) => {
-  res.sendFile(__path + '/qr_page.html')
+  res.sendFile(__path + '/qr.html')
   })
   app.use('/pair', async (req, res, next) => {
   res.sendFile(__path + '/pair.html')
